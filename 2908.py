f2521ea44d7161295e457f46e958c6034cb6065f
eunbinni <dnflrhra@naver.com>
2024-04-21 19:52:17 +0900

    0421 구현

---
+++ 2908.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+# a와 b를 string으로 받는다
+# a를 string 하나씩 자른다
+# 항상 세자리 수다
+A = int(a[2] + a[1] + a[0])
+B = int(b[2] + b[1] + b[0])
+
+if A > B:
+    print(A)
+else:
+    print(B)
