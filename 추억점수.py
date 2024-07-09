7fa0943816e60b6087c31ae4e222d44d2ff9adf0
eunbinni <dnflrhra@naver.com>
2024-07-10 00:14:00 +0900

    240710

---
+++ 추억점수.py
@@ -0,0 +1,18 @@
+def solution(name, yearning, photo):
+    answer = []
+
+    # 딕셔너리 만들기
+    think_idx = {}
+    for i in range(len(name)):
+        think_idx[name[i]] = yearning[i]
+
+    for row in photo:
+        cnt = 0
+        for person in row:
+            if person in name:
+                cnt += think_idx[person]
+            else:
+                pass
+        answer.append(cnt)
+
+    return answer
