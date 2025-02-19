34af6563250dc8f27d1c601f014804c926005a40
eunbinni <dnflrhra@naver.com>
2025-02-19 21:20:51 +0900

    250219 queue

---
+++ datastructure/42586_programmers.py
@@ -0,0 +1,23 @@
+# 기능 개발
+# 두가지 접근법
+
+def solution(progresses, speeds):
+    answer = []
+    time = 0
+    count = 0
+
+    while len(progresses) > 0:
+        if (progresses[0] + time * speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
+
+solution([93, 30, 55], [1, 30, 5]) # [2,1]
+solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1]) # [1,3,2]
