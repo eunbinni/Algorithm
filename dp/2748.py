e033b8dad8d5f64488a5348696f3a2d8669fe890
eunbinni <dnflrhra@naver.com>
2025-03-10 17:19:51 +0900

    250310 recursion memoization

--- dp/2748.py
+++ dp/2748.py
@@ -8,10 +8,13 @@ for i in range(2, n+1):
     dp[i] = dp[i-1] + dp[i-2]
     # print('i:', i, dp[i])
 print(dp[n])
-#
-# # recursion
-# def fib(n):
-#     if n<2:
-#         return n
-#     return fib(n-1) + fib(n-2)
-# print(fib(n))
+
+# recursion
+def fib(n):
+    if dp[n]>0:
+        return dp[n] # 값 있으면 바로 리턴, 가지 뻗지 말고
+    if n<2:
+        return n
+    dp[n] = fib(n-1) + fib(n-2)
+    return dp[n]
+print(fib(n))
