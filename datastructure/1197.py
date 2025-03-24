c8eec6d06d05ca9440804c51c42fe982e1080ba2
eunbinni <dnflrhra@naver.com>
2025-03-24 12:13:43 +0900

    250324 graph

--- datastructure/1197.py
+++ datastructure/1197.py
@@ -5,20 +5,20 @@ input = sys.stdin.readline
 v, e = map(int, input().split())
 graph = [[] for _ in range(v+1)]
 visited = [False] * (v+1)
-# create adj list
-for _ in range(v+1):
+result = 0
+for i in range(e):
     a, b, c = map(int, input().split())
-    graph[a].append([c, b]) # (edge, vertex)
+    graph[a].append([c, b])
     graph[b].append([c, a])
 
-heap = [[0,1]]
-result = 0
+heap = [[0, 1]]
+visited[0] = True
 while heap:
-    w, each_node = heapq.heappop(heap)
-    if not visited[each_node]:
-        visited[each_node] = True
+    w, n = heapq.heappop(heap)
+    if not visited[n]:
+        visited[n] = True
         result += w
-        for next_edge in graph[each_node]: # 노드에 붙은 간선 확인
-            if not visited[next_edge[1]]:
-                heapq.heappush(heap, next_edge)
+        for next_e in graph[n]:
+            if not visited[next_e[1]]:
+                heapq.heappush(heap, next_e)
 print(result)
